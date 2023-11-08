2d7c3296af76796e406c6cfdf70c31a0fbabc375
Levi Bostian <levi@levi.earth>
2023-11-08 09:08:25 -0600

    fix: download sd v1.0

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,18 @@ async function main() {
     version = latestRelease.data.tag_name.replace("v", "");
   }
 
-  const targetPlatform = core.getInput("target-platform");
+  const targetPlatform = core.getInput("target-platform");    
 
   let cachedPath = tc.find("sd", version);
   if (!cachedPath) {
     const url = `https://github.com/chmln/sd/releases/download/v${version}/sd-v${version}-${targetPlatform}`;
+
+    // sd began adding a file extension starting on version v1.0.0
+    // Add the file extension unless the version is < v1.0.0
+    if (!version.startsWith("0.") {
+      url += ".tar.gz";
+    }
+    
     const binPath = await tc.downloadTool(url);
     cachedPath = await tc.cacheFile(binPath, "sd", "sd", version);
   }
