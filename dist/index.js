59a1bd7bba077f6c491f04cd9d24b524ea2db4b6
Levi Bostian <levi@levi.earth>
2023-11-09 08:22:24 -0600

    fix: download sd v1.0 (#35)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -13284,11 +13284,23 @@ async function main() {
         });
         version = latestRelease.data.tag_name.replace("v", "");
     }
+    const isVersionPreVersion1 = version.startsWith("0.");
     const targetPlatform = core.getInput("target-platform");
     let cachedPath = tc.find("sd", version);
     if (!cachedPath) {
-        const url = `https://github.com/chmln/sd/releases/download/v${version}/sd-v${version}-${targetPlatform}`;
-        const binPath = await tc.downloadTool(url);
+        let url = `https://github.com/chmln/sd/releases/download/v${version}/sd-v${version}-${targetPlatform}`;
+        // sd began adding a file extension starting on version v1.0.0
+        // Add the file extension unless the version is < v1.0.0
+        if (!isVersionPreVersion1) {
+            url += ".tar.gz";
+        }
+        let binPath = await tc.downloadTool(url);
+        // Because we downloaded a .tar.gz, we must extract it and then find the sd binary file within the extracted directory
+        if (!isVersionPreVersion1) {
+            const extractedTarDestination = await tc.extractTar(binPath);
+            // Update binPath to point to the sd binary file within the extracted directory
+            binPath = `${extractedTarDestination}/sd-v${version}-${targetPlatform}/sd`;
+        }
         cachedPath = await tc.cacheFile(binPath, "sd", "sd", version);
     }
     await (0, child_process_1.exec)(`chmod +x ${cachedPath}/sd`);
